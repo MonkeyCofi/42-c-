0f07877075ee6ba96f2fee65e2ec5547bed9afbb
MonkeyCofi <piercediet06@gmail.com>
2024-08-29 21:04:29 +0400

    added a makefile script

--- cpp.sh
+++ cpp.sh
@@ -2,6 +2,39 @@
 
 NAME=""
 
-echo -n "Program name: "
+SRCS=""
 
-read -a NAME
+read -p "Program name: " NAME
+
+read -p "Program sources: " SRCS
+
+touch ./Makefile
+
+echo 	\
+"NAME = $NAME
+
+CXX = c++
+
+CXXFLAGS = -Wall -Wextra -Werror -std=c++98
+
+SRCS = $SRCS
+
+OBJS = \$(SRCS:.cpp=.o)
+
+%.o: %.cpp
+	\$(CXX) \$(CXXFLAGS) \$< -o \$^
+	
+\$(NAME): \$(OBJS)
+	\$(CXX) \$(CXXFLAGS) \$(OBJS) -o \$(NAME)
+
+all: \$(NAME)
+
+clean:
+	rm -f \$(OBJS)
+	
+fclean: clean
+	rm -f \$(NAME)
+	
+re: fclean all
+
+.PHONY: all clean fclean re" > ./Makefile
--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pipolint <pipolint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 12:24:56 by pipolint          #+#    #+#             */
-/*   Updated: 2024/08/29 12:30:25 by pipolint         ###   ########.fr       */
+/*   Updated: 2024/08/29 16:52:06 by pipolint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,8 @@ int main(void)
 	std::cout << "String's memory: " << &str << std::endl;
 	std::cout << "Pointer to the string: " << stringPTR << std::endl;
 	std::cout << "Reference to the string: " << &stringREF << std::endl;
+	
+	std::cout << "String's value: " << str << std::endl;
+	std::cout << "Pointer's value: " << *stringPTR << std::endl;
+	std::cout << "Reference's value: " << stringREF << std::endl;
 }
