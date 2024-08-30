dcf98801cb92355fc7733b7d075835ef77a59257
MonkeyCofi <piercediet06@gmail.com>
2024-08-30 21:54:20 +0400

    slight changes to cpp01

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -6,11 +6,21 @@
 /*   By: pipolint <pipolint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 21:08:56 by pipolint          #+#    #+#             */
-/*   Updated: 2024/08/29 21:21:33 by pipolint         ###   ########.fr       */
+/*   Updated: 2024/08/30 12:35:51 by pipolint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
+
+Weapon()
+{
+	;
+};
+
+~Weapon()
+{
+	;
+};
 
 const std::string	Weapon::getType()
 {
