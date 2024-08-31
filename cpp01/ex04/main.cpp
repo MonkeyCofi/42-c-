a1cfbb1db78f4098abc5f28f1679ae4fdc30cd2d
MonkeyCofi <piercediet06@gmail.com>
2024-08-31 18:29:31 +0400

    finished cpp01/ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pipolint <pipolint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 14:28:14 by pipolint          #+#    #+#             */
-/*   Updated: 2024/08/31 17:34:22 by pipolint         ###   ########.fr       */
+/*   Updated: 2024/08/31 18:29:20 by pipolint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,17 @@ int main(int ac, char **av)
 		found_pos = line.find(std::string(av[2]));
 		for (int i = 0; line[i]; i++)
 		{
-			found_pos = line.find(std::string(av[2]));
-			if (found_pos != (size_t)-1)
+			found_pos = line.substr(i, std::string::npos).find(std::string(av[2]));
+			if (found_pos != std::string::npos)
 			{
 				line.erase(found_pos, std::string(av[2]).length());
-				line.insert(found_pos, std::string(av[2]));
+				line.insert(found_pos, std::string(av[3]));
 				i += found_pos;
 			}
 		}
 		replace_file << line;
 		replace_file << std::endl;
 	}
+	replace_file.close();
+	infile.close();
 }
